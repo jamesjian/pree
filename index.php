cf54e87d98a52b02e5222046de63a59450615da9
jamesjian <james.jian.hang@gmail.com>
2012-10-13 18:24:27 +1100

    live

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 ob_start("ob_gzhandler");
 define('PHP_ROOT', __DIR__ . DIRECTORY_SEPARATOR);  
-
+//echo PHP_ROOT;
 include PHP_ROOT . 'application/config/constant.php';
 
 
 include SYSTEM_PATH . 'autoloader.php';
 
-//\Zx\Test\Test::object_log('lob', $_SERVER, __FILE__, __LINE__, __CLASS__, __METHOD__);
+\Zx\Test\Test::object_log('lob', $_SERVER, __FILE__, __LINE__, __CLASS__, __METHOD__);
 \Zx\Controller\Application::run();
 ob_end_flush();
