e178515d68143596f9c507e6e678062ac4a511ff
james <james@james.com>
2012-10-17 16:50:24 +1100

    cron

--- application/module/cron/c1.php
+++ application/module/cron/c1.php
@@ -7,11 +7,15 @@
 include 'cron_head.php';
 
 $description = Article::backup_sql();
+$backup_path = PHP_ROOT . 'upload/backup/';
+$file = $backup_path . 'article_' . date('Y-m-d') . '.php'; 
+file_put_contents($file, $description);
 //\Zx\Test\Test::object_log('$description', $description, __FILE__, __LINE__, __CLASS__, __METHOD__);      
 $message = Swift_Message::newInstance();
 $message->setSubject('article backup')
         ->setFrom(array(MAIL_USER => 'baoxian.com.au'))
         ->setTo(array('james.jian.hang@gmail.com'))
-        ->setBody($description, 'text/html');
+        //->setBody($description, 'text/html')  //use this to send text to email box
+        ->attach(Swift_Attachment::fromPath($file));  //use this to send attachment to email box
 Swiftmail::send_email($message);
 
